743d7a120e6340185008a4da02d2a888b84c32a0
Samuel Dalvai <s.dalvai@ewo.com>
2024-12-23 16:25:27 +0100

    feat: add blueprint for game class

---
+++ index.ts
@@ -0,0 +1,7 @@
+import Game from "./src/game/Game";
+
+const game = new Game()
+
+game.initialize()
+game.run()
+game.destroy()
---
+++ src/game/Game.ts
@@ -0,0 +1,18 @@
+export default class Game {
+
+
+
+    initialize = () => {
+        console.log("Initializing game");
+    }
+
+    run = () => {
+        console.log("Running game")
+
+    }
+
+    destroy = () => {
+        console.log("Destroying game");
+
+    }
+}
