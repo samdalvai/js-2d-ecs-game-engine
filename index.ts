b614f2d619a3e04ab6ac464df5404394d1bf7b85
Samuel Dalvai <s.dalvai@ewo.com>
2024-12-23 17:03:48 +0100

    feat: implement run method of Game class and limit fps to 60 when running Game update

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import Game from "./src/game/Game";
 
-const game = new Game()
+const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+if (!ctx) {
+    throw new Error("Failed to get 2D context for the canvas.");
+}
+
+const game = new Game(canvas, ctx)
 
 game.initialize()
 game.run()
--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,12 +1,19 @@
+import { sleep } from '../utils/time';
+
+const FPS = 60;
+const MILLISECS_PER_FRAME = 1000 / FPS;
+
 export default class Game {
     isRunning: boolean;
-    canvas: HTMLCanvasElement | null;
-    ctx: CanvasRenderingContext2D | null;
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    millisecsPreviousFrame = 0;
+    millisecondsLastFPSUpdate = 0;
 
-    constructor() {
+    constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
         this.isRunning = false;
-        this.canvas = null;
-        this.ctx = null;
+        this.canvas = canvas;
+        this.ctx = ctx;
     }
 
     initialize = () => {
@@ -23,13 +30,53 @@ export default class Game {
         this.isRunning = true;
     };
 
+    setup = () => {};
+
+    processInput = () => {};
+
+    update = async () => {
+        // If we are too fast, waste some time until we reach the MILLISECS_PER_FRAME
+        const timeToWait = MILLISECS_PER_FRAME - (performance.now() - this.millisecsPreviousFrame);
+        if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+            await sleep(timeToWait);
+        }
+
+        // The difference in ticks since the last frame, converted to seconds
+        const deltaTime = (performance.now() - this.millisecsPreviousFrame) / 1000.0;
+
+        const millisecsCurrentFrame = performance.now();
+        if (millisecsCurrentFrame - this.millisecondsLastFPSUpdate >= 1000) {
+            const currentFPS = 1000 / (millisecsCurrentFrame - this.millisecsPreviousFrame);
+            this.millisecondsLastFPSUpdate = millisecsCurrentFrame;
+            console.log('FPS: ' + currentFPS);
+        }
+
+        this.millisecsPreviousFrame = performance.now();
+    };
+
+    render = () => {
+        // Clear the whole canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // Render entities
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(100, 100, 50, 50);
+    };
+
     run = () => {
         console.log('Running game');
+        this.setup();
+        const loop = async () => {
+            while (this.isRunning) {
+                this.processInput();
+                await this.update();
+                this.render();
+            }
+        };
+        loop();
     };
 
     destroy = () => {
         console.log('Destroying game');
-        this.canvas = null;
-        this.ctx = null;
     };
 }
