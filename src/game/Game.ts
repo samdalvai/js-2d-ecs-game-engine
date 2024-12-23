21d3f7be5cc0fb8e03d19c4e34312f98b345e355
Samuel Dalvai <s.dalvai@ewo.com>
2024-12-23 16:33:33 +0100

    feat: initialize canvas element and 2d context

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,18 +1,35 @@
 export default class Game {
+    isRunning: boolean;
+    canvas: HTMLCanvasElement | null;
+    ctx: CanvasRenderingContext2D | null;
 
-
+    constructor() {
+        this.isRunning = false;
+        this.canvas = null;
+        this.ctx = null;
+    }
 
     initialize = () => {
-        console.log("Initializing game");
-    }
+        console.log('Initializing game');
+        const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d');
+
+        if (!ctx) {
+            throw new Error('Failed to get 2D context for the canvas.');
+        }
+
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.isRunning = true;
+    };
 
     run = () => {
-        console.log("Running game")
-
-    }
+        console.log('Running game');
+    };
 
     destroy = () => {
-        console.log("Destroying game");
-
-    }
+        console.log('Destroying game');
+        this.canvas = null;
+        this.ctx = null;
+    };
 }
