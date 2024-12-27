404d5e4c1c25deb885c121b8ac6cc1c86b6ea658
Samuel Dalvai <s.dalvai@ewo.com>
2024-12-27 17:45:03 +0100

    test: add unit tests on Registry methods

---
+++ __tests__/Registry.test.ts
@@ -0,0 +1,35 @@
+import { expect } from '@jest/globals';
+
+import Entity from '../src/ecs/Entity';
+import Registry from '../src/ecs/Registry';
+
+describe('Testing Registry related functions', () => {
+    test('Should add entity to registry', () => {
+        const registry = new Registry();
+        const entity = registry.createEntity();
+
+        expect(entity.getId()).toBe(0);
+        expect(entity.registry).toEqual(registry);
+        expect(registry.entitiesToBeAdded.size).toBe(1);
+    });
+
+    test('Should kill entity', () => {
+        const registry = new Registry();
+        const entity = new Entity(1);
+        registry.killEntity(entity);
+
+        expect(registry.entitiesToBeKilled.size).toBe(1);
+    });
+
+    test('Should create entity with id from the free ids', () => {
+        const registry = new Registry();
+        registry.freeIds = [999];
+
+        const entity = registry.createEntity();
+
+        expect(entity.getId()).toBe(999);
+        expect(entity.registry).toEqual(registry);
+        expect(registry.entitiesToBeAdded.size).toBe(1);
+        expect(registry.freeIds.length).toBe(0);
+    });
+});
