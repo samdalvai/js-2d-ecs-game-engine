dd7217eb97894ea306cec54d9bf3705c8fe6d3f3
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-02 17:20:00 +0100

    docs: add todo comment on sprite flipping

--- src/components/SpriteComponent.ts
+++ src/components/SpriteComponent.ts
@@ -1,6 +1,7 @@
 import Component from '../ecs/Component';
 import { Rect } from '../types';
 
+// TODO: handle flipping images and rotation, if possible
 export default class SpriteComponent extends Component {
     assetId: string;
     width: number;
@@ -27,22 +28,3 @@ export default class SpriteComponent extends Component {
         this.isFixed = isFixed;
     }
 }
-
-/*
-std::string assetId;
-    int width;
-    int height;
-    int zIndex;
-    SDL_RendererFlip flip;
-    bool isFixed;
-    SDL_Rect srcRect;
-    
-    SpriteComponent(std::string assetId = "", int width = 0, int height = 0, int zIndex = 0, bool isFixed = false, int srcRectX = 0, int srcRectY = 0) {
-        this->assetId = assetId;
-        this->width = width;
-        this->height = height;
-        this->zIndex = zIndex;
-        this->flip = SDL_FLIP_NONE;
-        this->isFixed = isFixed;
-        this->srcRect = {srcRectX, srcRectY, width, height};
-    }*/
