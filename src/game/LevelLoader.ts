f36159c3cce4b74a3f85ed73be78aa830f1b7c24
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-02 22:18:57 +0100

    fix: compute map height based on row number instead of column number

--- src/game/LevelLoader.ts
+++ src/game/LevelLoader.ts
@@ -67,14 +67,14 @@ export default class LevelLoader {
         }
 
         Game.mapWidth = columnNumber * tileSize * mapScale;
-        Game.mapHeight = columnNumber * tileSize * mapScale;
+        Game.mapHeight = rowNumber * tileSize * mapScale;
     }
 
     private loadEntities(registry: Registry) {
         const player = registry.createEntity();
         player.addComponent(TransformComponent, { x: 100, y: 100 }, { x: 1, y: 1 }, 0);
         player.addComponent(SpriteComponent, 'chopper-texture', 32, 32, 1, 0, 0);
-        player.addComponent(RigidBodyComponent, { x: 30, y: 30 });
+        player.addComponent(RigidBodyComponent, { x: 60, y: 100 });
         player.addComponent(CameraFollowComponent);
 
         const enemy = registry.createEntity();
