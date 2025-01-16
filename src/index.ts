8f6c8c4e2047f0e4fb0d5d8d6df1b0d99591e70b
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-16 21:09:43 +0100

    refactor: remove app div element from index.html

--- src/index.ts
+++ src/index.ts
@@ -6,37 +6,37 @@ game.initialize();
 game.run();
 game.destroy();
 
-const image = new Image();
-image.src = './assets/chopper.png';
+// const image = new Image();
+// image.src = './assets/chopper.png';
 
-image.onload = () => {
-    console.log('Image loaded: ', image.src);
-};
+// image.onload = () => {
+//     console.log('Image loaded: ', image.src);
+// };
 
-image.onerror = () => {
-    console.error('Failed to load the image: ', image.src);
-};
+// image.onerror = () => {
+//     console.error('Failed to load the image: ', image.src);
+// };
 
-console.log('image: ', image);
+// console.log('image: ', image);
 
-const sound = new Audio();
-sound.src = './assets/helicopter.wav';
+// const sound = new Audio();
+// sound.src = './assets/helicopter.wav';
 
-function onPlay() {
-    sound.play();
-}
+// function onPlay() {
+//     sound.play();
+// }
 
-const app = document.getElementById('app');
-if (app) {
-    app.innerHTML = '<h1>Hello, TypeScriptssss Hello!</h1>';
-    app.appendChild(image);
+// const app = document.getElementById('app');
+// if (app) {
+//     app.innerHTML = '<h1>Hello, TypeScriptssss Hello!</h1>';
+//     app.appendChild(image);
 
-    const button = document.createElement('button');
-    button.innerHTML = 'Click Me!';
+//     const button = document.createElement('button');
+//     button.innerHTML = 'Click Me!';
 
-    // Add the onclick event listener to the button
-    button.onclick = onPlay;
+//     // Add the onclick event listener to the button
+//     button.onclick = onPlay;
 
-    // Append the button to the app
-    app.appendChild(button);
-}
+//     // Append the button to the app
+//     app.appendChild(button);
+// }
