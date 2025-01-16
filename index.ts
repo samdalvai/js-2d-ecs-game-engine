fe883d9754d11582661cf00f19dd157a54565113
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-16 17:51:58 +0100

    test: put chopper sprite in dist folder

--- index.ts
+++ index.ts
@@ -6,7 +6,13 @@ game.initialize();
 game.run();
 game.destroy();*/
 
+const image = new Image();
+image.src = './assets/chopper.png';
+
+console.log('image: ', image);
+
 const app = document.getElementById('app');
 if (app) {
     app.innerHTML = `<h1>Hello, TypeScriptssss!</h1>`;
+    app.appendChild(image);
 }
