3b5a86c6e26bda0356ddc0c0499a3911beb886d0
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-16 18:01:05 +0100

    test: load sound file with onclick event on a button

--- index.ts
+++ index.ts
@@ -11,8 +11,24 @@ image.src = './assets/chopper.png';
 
 console.log('image: ', image);
 
+const sound = new Audio();
+sound.src = './assets/helicopter.wav';
+
+function onPlay() {
+    sound.play();
+}
+
 const app = document.getElementById('app');
 if (app) {
     app.innerHTML = `<h1>Hello, TypeScriptssss!</h1>`;
     app.appendChild(image);
+
+    const button = document.createElement('button');
+    button.innerHTML = 'Click Me!';
+
+    // Add the onclick event listener to the button
+    button.onclick = onPlay;
+
+    // Append the button to the app
+    app.appendChild(button);
 }
