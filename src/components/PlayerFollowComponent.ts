e9775fce00a206d4cd3b8920585b7b433d7b3663
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-17 09:49:16 +0100

    feat: add velocity of follow component

--- src/components/PlayerFollowComponent.ts
+++ src/components/PlayerFollowComponent.ts
@@ -4,12 +4,14 @@ import { Vector } from '../types';
 export default class PlayerFollowComponent extends Component {
     detectionRadius: number;
     minFollowDistance: number;
+    followVelocity: number;
     offset: Vector;
 
-    constructor(detectionRadius = 0, minFollowDistance = 0, offset = { x: 0, y: 0 }) {
+    constructor(detectionRadius = 0, minFollowDistance = 0, followVelocity = 0, offset = { x: 0, y: 0 }) {
         super();
         this.detectionRadius = detectionRadius;
         this.minFollowDistance = minFollowDistance;
+        this.followVelocity = followVelocity;
         this.offset = offset;
     }
 }
