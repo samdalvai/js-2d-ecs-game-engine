d2194272d66cf4ea6ea679582a60ed82c100fcd7
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-26 14:07:43 +0100

    test: add unit tests on entity follow system when entity is aligned and within min follow distance

--- __tests__/systems/PlayerFollowSystem.test.ts
+++ __tests__/systems/PlayerFollowSystem.test.ts
@@ -8,6 +8,10 @@ import Registry from '../../src/ecs/Registry';
 import PlayerFollowSystem from '../../src/systems/PlayerFollowSystem';
 
 describe('Testing Player follow system related functions', () => {
+    ////////////////////////////////////////////////////////////////////////////////
+    // Player in a corner
+    ////////////////////////////////////////////////////////////////////////////////
+
     test('Entity should move right if player is in the top right corner', () => {
         const registry = new Registry();
 
@@ -108,6 +112,10 @@ describe('Testing Player follow system related functions', () => {
         expect(rigidBody?.direction).toEqual({ x: -1, y: 0 });
     });
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // Player on same axis
+    ////////////////////////////////////////////////////////////////////////////////
+
     test('Entity should move up if player is aligned in the x axis and below player', () => {
         const registry = new Registry();
 
@@ -205,6 +213,110 @@ describe('Testing Player follow system related functions', () => {
         const rigidBody = entity.getComponent(RigidBodyComponent);
 
         expect(rigidBody?.velocity.x).toBe(-50);
+        expect(rigidBody?.direction).toEqual({ x: -1, y: 0 });
+    });
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Player on same axis but nearer than min distance
+    ////////////////////////////////////////////////////////////////////////////////
+
+    test('Entity should not move up if player is aligned in the x axis and below player and within min follow distance', () => {
+        const registry = new Registry();
+
+        const entity = registry.createEntity();
+        entity.addComponent(TransformComponent, { x: 500, y: 450 }, { x: 1, y: 1 }, 0);
+        entity.addComponent(RigidBodyComponent, { x: 0, y: 0 });
+        entity.addComponent(PlayerFollowComponent, 400, 100, 50, { x: 16, y: 16 });
+
+        const player = registry.createEntity();
+        player.addComponent(SpriteComponent, 'test', 32, 32);
+        player.addComponent(TransformComponent, { x: 500, y: 400 }, { x: 1, y: 1 }, 0);
+        player.tag('player');
+
+        registry.addSystem(PlayerFollowSystem);
+
+        registry.update();
+
+        registry.getSystem(PlayerFollowSystem)?.update(registry);
+
+        const rigidBody = entity.getComponent(RigidBodyComponent);
+
+        expect(rigidBody?.velocity.y).toBe(0);
+        expect(rigidBody?.direction).toEqual({ x: 0, y: -1 });
+    });
+
+    test('Entity should not move down if player is aligned in the x axis and above player and within min follow distance', () => {
+        const registry = new Registry();
+
+        const entity = registry.createEntity();
+        entity.addComponent(TransformComponent, { x: 500, y: 400 }, { x: 1, y: 1 }, 0);
+        entity.addComponent(RigidBodyComponent, { x: 0, y: 0 });
+        entity.addComponent(PlayerFollowComponent, 400, 100, 50, { x: 16, y: 16 });
+
+        const player = registry.createEntity();
+        player.addComponent(SpriteComponent, 'test', 32, 32);
+        player.addComponent(TransformComponent, { x: 500, y: 450 }, { x: 1, y: 1 }, 0);
+        player.tag('player');
+
+        registry.addSystem(PlayerFollowSystem);
+
+        registry.update();
+
+        registry.getSystem(PlayerFollowSystem)?.update(registry);
+
+        const rigidBody = entity.getComponent(RigidBodyComponent);
+
+        expect(rigidBody?.velocity.y).toBe(0);
+        expect(rigidBody?.direction).toEqual({ x: 0, y: 1 });
+    });
+
+    test('Entity should not move right if player is aligned in the y axis and on the left of the player and within min follow distance', () => {
+        const registry = new Registry();
+
+        const entity = registry.createEntity();
+        entity.addComponent(TransformComponent, { x: 400, y: 500 }, { x: 1, y: 1 }, 0);
+        entity.addComponent(RigidBodyComponent, { x: 0, y: 0 });
+        entity.addComponent(PlayerFollowComponent, 400, 100, 50, { x: 16, y: 16 });
+
+        const player = registry.createEntity();
+        player.addComponent(SpriteComponent, 'test', 32, 32);
+        player.addComponent(TransformComponent, { x: 450, y: 500 }, { x: 1, y: 1 }, 0);
+        player.tag('player');
+
+        registry.addSystem(PlayerFollowSystem);
+
+        registry.update();
+
+        registry.getSystem(PlayerFollowSystem)?.update(registry);
+
+        const rigidBody = entity.getComponent(RigidBodyComponent);
+
+        expect(rigidBody?.velocity.x).toBe(0);
+        expect(rigidBody?.direction).toEqual({ x: 1, y: 0 });
+    });
+
+    test('Entity should not move left if player is aligned in the y axis and on the right of the player and within min follow distance', () => {
+        const registry = new Registry();
+
+        const entity = registry.createEntity();
+        entity.addComponent(TransformComponent, { x: 450, y: 500 }, { x: 1, y: 1 }, 0);
+        entity.addComponent(RigidBodyComponent, { x: 0, y: 0 });
+        entity.addComponent(PlayerFollowComponent, 400, 100, 50, { x: 16, y: 16 });
+
+        const player = registry.createEntity();
+        player.addComponent(SpriteComponent, 'test', 32, 32);
+        player.addComponent(TransformComponent, { x: 400, y: 500 }, { x: 1, y: 1 }, 0);
+        player.tag('player');
+
+        registry.addSystem(PlayerFollowSystem);
+
+        registry.update();
+
+        registry.getSystem(PlayerFollowSystem)?.update(registry);
+
+        const rigidBody = entity.getComponent(RigidBodyComponent);
+
+        expect(rigidBody?.velocity.x).toBe(0);
         expect(rigidBody?.direction).toEqual({ x: -1, y: 0 });
     });
 });
