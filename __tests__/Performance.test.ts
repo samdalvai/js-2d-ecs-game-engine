0ca55458417733bc019a20a174905048acf2e26c
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-26 14:33:13 +0100

    test: repeat performance test multiple times

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,10 +1,31 @@
 describe('Testing performance related functions', () => {
+    enum Test {
+        TEST_1,
+        TEST_2,
+    }
+
     test('A test on performance', () => {
-        const start = performance.now();
+        const times: number[] = [];
 
-        // Code to be tested
+        for (let x = 0; x < 10; x++) {
+            const start = performance.now();
 
-        const end = performance.now();
-        console.log('Time elapsed: ', end - start);
+            // Code to be tested
+            let a = 0;
+            for (let i = 0; i < 1000000; i++) {
+                if (Test.TEST_1 === Test.TEST_1) {
+                    a++;
+                }
+            }
+
+            const end = performance.now();
+            times.push(end - start);
+        }
+
+        let sum = 0;
+        for (let x = 0; x < times.length; x++) {
+            sum += times[x];
+        }
+        console.log('Time elapsed: ' + sum / times.length + ' seconds');
     });
 });
