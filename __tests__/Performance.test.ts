e92fcd9d54556c54fc7556fe4fccc69532523827
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-26 14:37:15 +0100

    test: clean up performance test code

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,9 +1,4 @@
 describe('Testing performance related functions', () => {
-    enum Test {
-        TEST_1,
-        TEST_2,
-    }
-
     test('A test on performance', () => {
         const times: number[] = [];
 
@@ -11,12 +6,6 @@ describe('Testing performance related functions', () => {
             const start = performance.now();
 
             // Code to be tested
-            let a = 0;
-            for (let i = 0; i < 1000000; i++) {
-                if (Test.TEST_1 === Test.TEST_1) {
-                    a++;
-                }
-            }
 
             const end = performance.now();
             times.push(end - start);
@@ -26,6 +15,6 @@ describe('Testing performance related functions', () => {
         for (let x = 0; x < times.length; x++) {
             sum += times[x];
         }
-        console.log('Time elapsed: ' + sum / times.length + ' seconds');
+        console.log('Time elapsed: ' + sum / times.length + ' ms');
     });
 });
