011ba6e75f8b5ce160ede2b16f4561e3f2b946b0
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-26 14:29:10 +0100

    test: add time elapsed to performance test

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,9 +1,10 @@
-import { expect } from '@jest/globals';
-
 describe('Testing performance related functions', () => {
     test('A test on performance', () => {
-        
+        const start = performance.now();
 
+        // Code to be tested
 
+        const end = performance.now();
+        console.log('Time elapsed: ', end - start);
     });
 });
