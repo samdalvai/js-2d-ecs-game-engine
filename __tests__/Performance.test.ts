b0d45cb8121454a0f575710ef39fc93a99e724ab
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-26 14:44:23 +0100

    test: add test on javascript map with enums, strings and ints

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,4 +1,24 @@
 describe('Testing performance related functions', () => {
+    enum Test {
+        TEST1,
+        TEST2,
+    }
+
+    const myMapEnum = new Map<Test, string>();
+
+    myMapEnum.set(Test.TEST1, 'Hello1');
+    myMapEnum.set(Test.TEST2, 'Hello2');
+
+    const myMapString = new Map<string, string>();
+
+    myMapString.set('test1', 'Hello1');
+    myMapString.set('test2', 'Hello2');
+
+    const myMapInts = new Map<number, string>();
+
+    myMapInts.set(1, 'Hello1');
+    myMapInts.set(2, 'Hello2');
+
     test('A test on performance', () => {
         const times: number[] = [];
 
@@ -7,7 +27,14 @@ describe('Testing performance related functions', () => {
 
             // Code to be tested
             for (let i = 0; i < 100000000; i++) {
-                //
+                // myMapEnum.get(Test.TEST1);
+                // myMapEnum.get(Test.TEST2);
+
+                myMapString.get('string1');
+                myMapString.get('string2');
+
+                // myMapInts.get(1);
+                // myMapInts.get(2);
             }
 
             const end = performance.now();
