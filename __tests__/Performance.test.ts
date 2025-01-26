86252eeb86185dd4a57b24b81e47e557a233d3cf
Samuel Dalvai <s.dalvai@ewo.com>
2025-01-26 14:49:24 +0100

    chore: remove test code for performance testing

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,24 +1,4 @@
 describe('Testing performance related functions', () => {
-    enum Test {
-        TEST1,
-        TEST2,
-    }
-
-    const myMapEnum = new Map<Test, string>();
-
-    myMapEnum.set(Test.TEST1, 'Hello1');
-    myMapEnum.set(Test.TEST2, 'Hello2');
-
-    const myMapString = new Map<string, string>();
-
-    myMapString.set('test1', 'Hello1');
-    myMapString.set('test2', 'Hello2');
-
-    const myMapInts = new Map<number, string>();
-
-    myMapInts.set(1, 'Hello1');
-    myMapInts.set(2, 'Hello2');
-
     test('A test on performance', () => {
         const times: number[] = [];
 
@@ -26,15 +6,8 @@ describe('Testing performance related functions', () => {
             const start = performance.now();
 
             // Code to be tested
-            for (let i = 0; i < 100000000; i++) {
-                // myMapEnum.get(Test.TEST1);
-                // myMapEnum.get(Test.TEST2);
-
-                myMapString.get('string1');
-                myMapString.get('string2');
-
-                // myMapInts.get(1);
-                // myMapInts.get(2);
+            for (let i = 0; i < 1000; i++) {
+                //
             }
 
             const end = performance.now();
@@ -45,6 +18,6 @@ describe('Testing performance related functions', () => {
         for (let x = 0; x < times.length; x++) {
             sum += times[x];
         }
-        console.log('Time elapsed: ' + sum / times.length + ' ms');
+        // console.log('Time elapsed: ' + sum / times.length + ' ms');
     });
 });
